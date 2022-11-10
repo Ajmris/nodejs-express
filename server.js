6507db170fb1a2e00d3299ad1e657f62c71e129c
ajmris <szymektarasiewicz@onet.pl>
2022-11-10 22:52:18 +0100

    setup routes

--- server.js
+++ server.js
@@ -25,43 +25,12 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-//Te get'ery zwracają pliki html bez styli
-app.get('^/$|index(.html)?', (req, res)=>{
-    // '^/$|index.html' to oznacza, że jak wpiszę index.html
-    //albo nic po / to res zwróci to samo
-    //res.sendFile('./views/index.html', {root: __dirname});
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-app.get('/new-page(.html)?', (req, res)=>{
-    //res.sendFile('./views/index.html', {root: __dirname});
-    //wzięcie rozszerzenia w nawias sprawi, że w wyszukiwarce nie trzeba go wpisywać
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-app.get('/old-page(.html)?', (req, res)=>{
-    res.redirect(301, 'new-page.html');
-});
-//Route handlers
-app.get('/hello(.html)?', (req, res, next)=>{
-    console.log('załącznik do odpalenia hello.html');
-    next()
-},(req, res)=>{
-    res.send('Hello World!');
-})
-// łańcuch uchwytów połączeń
-const one = (req, res, next) => {
-    console.log('one');
-    next();
-}
-const two = (req, res, next) => {
-    console.log('two');
-    next();
-}
-const three = (req, res) => {
-    console.log('three');
-    res.send('Finished!');
-}
-
-app.get('/chain(.html)?', [one, two, three]);
+// serwowanie plików statycznych
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
 
 app.all('/*', (req, res)=>{
     res.status(404);
